a1d1ddd66b9bcd41264feb42ba3b9cf046d82e14
Rico Derks <r.j.e.derks@lumc.nl>
2022-09-28 14:30:42 +0000

    Some cleanup.

--- app.R
+++ app.R
@@ -3,7 +3,6 @@ library(shinydashboard)
 library(shinydashboardPlus)
 library(shinyWidgets)
 library(shinyjqui)
-library(shinyjs)
 library(plotly)
 
 ui <- dashboardPage(
@@ -13,180 +12,24 @@ ui <- dashboardPage(
   #### sidebar menu ####
   dashboardSidebar(
     sidebarMenu(
-      menuItem(text = "Not movable", 
-               tabName = "notMovable", 
-               icon = icon("bar-chart-o")),
-      menuItem(text = "shinyjqui sortable", 
-               tabName = "shinyjquiSort", 
-               icon = icon("bar-chart-o")),
-      menuItem(text = "shinyjqui movable", 
-               tabName = "shinyjquiMovable", 
-               icon = icon("bar-chart-o")),
+      menuItem(text = "Dummy", 
+               tabName = "Dummy", 
+               icon = icon("file-excel")),
       menuItem(text = "shinyjqui Server side",
                tabName = "shinyjquiServer",
                icon = icon("bar-chart-o"))
-    ) # end sideBarMenu
+    )
   ),
   
   #### body ####
   dashboardBody(
-    # activate shinyjs
-    useShinyjs(),
     tabItems(
       tabItem(
-        tabName = "notMovable",
+        tabName = "Dummy",
         fluidRow(
-          checkboxGroupButtons(inputId = "showPlots1",
-                               label = "Show plots:",
-                               choices = c("Histogram" = "histogram",
-                                           "Histogram with controls" = "histogramCntrl",
-                                           "Plotly" = "plotly"),
-                               selected = c("histogram", "histogramCntrl", "plotly")),
-          p("Some times empty space!")
-        ), # end fluidrow 1
-        fluidRow(
-          # show plots
-          box(
-            id = "histogramBox1",
-            title = "Histogram",
-            solidHeader = TRUE,
-            collapsible = TRUE,
-            status = "primary",
-            plotOutput(outputId = "histPlot1")
-          ),
-          box(
-            id = "histogramCntrlBox1",
-            title = "Histogram with controls",
-            solidHeader = TRUE,
-            status = "primary",
-            collapsible = TRUE,
-            plotOutput(outputId = "histCntrlPlot1"),
-            sidebar = boxSidebar(
-              id = "hist_sidebar1",
-              width = 40,
-              sliderInput(inputId = "slider1",
-                          label = "Number of observations:",
-                          min = 1,
-                          max = 100,
-                          value = 50)
-            ) # end boxSidebar
-          ),
-          box(
-            id = "plotlyBox1",
-            title = "Plotly plot",
-            solidHeader = TRUE,
-            status = "primary",
-            collapsible = TRUE,
-            plotlyOutput(outputId = "plotlyPlot1")
-          )
-        ) # end fluidrow 2
-      ), # end notMovable
-      
-      tabItem(
-        tabName = "shinyjquiSort",
-        fluidRow(
-          checkboxGroupButtons(inputId = "showPlots2",
-                               label = "Show plots:",
-                               choices = c("Histogram" = "histogram",
-                                           "Histogram with controls" = "histogramCntrl",
-                                           "Plotly" = "plotly"),
-                               selected = c("histogram", "histogramCntrl", "plotly")),
-          p("Some times empty space. Can not zoom the plotly plot without moving the plot.")
-        ), # end fluidRow 1
-        fluidRow(
-          jqui_sortable(
-            div(
-              id = "movePlots",
-              box(
-                id = "histogramBox2",
-                title = "Histogram",
-                solidHeader = TRUE,
-                collapsible = TRUE,
-                status = "primary",
-                plotOutput(outputId = "histPlot2")
-              ),
-              box(
-                id = "histogramCntrlBox2",
-                title = "Histogram with controls",
-                solidHeader = TRUE,
-                status = "primary",
-                collapsible = TRUE,
-                plotOutput(outputId = "histCntrlPlot2"),
-                sidebar = boxSidebar(
-                  id = "hist_sidebar2",
-                  width = 40,
-                  sliderInput(inputId = "slider2",
-                              label = "Number of observations:",
-                              min = 1,
-                              max = 100,
-                              value = 50)
-                ) # end boxSidebar
-              ),
-              box(
-                id = "plotlyBox2",
-                title = "Plotly plot",
-                solidHeader = TRUE,
-                status = "primary",
-                collapsible = TRUE,
-                plotlyOutput(outputId = "plotlyPlot2")
-              )
-            ) # end div
-          ) # end jqui_sortable
-        ) # end fluidRow 2
-      ), # end tab shinyjquiSort
-      
-      tabItem(
-        tabName = "shinyjquiMovable",
-        fluidRow(
-          checkboxGroupButtons(inputId = "showPlots3",
-                               label = "Show plots:",
-                               choices = c("Histogram" = "histogram",
-                                           "Histogram with controls" = "histogramCntrl",
-                                           "Plotly" = "plotly"),
-                               selected = c("histogram", "histogramCntrl", "plotly")),
-          p("Same problem as sortable. Some times empty space. Can not zoom the plotly plot without moving the plot.")
-        ), # end fluidRow 1
-        fluidRow(
-          jqui_draggable(
-            div(
-              id = "movePlots",
-              box(
-                id = "histogramBox3",
-                title = "Histogram",
-                solidHeader = TRUE,
-                collapsible = TRUE,
-                status = "primary",
-                plotOutput(outputId = "histPlot3")
-              ),
-              box(
-                id = "histogramCntrlBox3",
-                title = "Histogram with controls",
-                solidHeader = TRUE,
-                status = "primary",
-                collapsible = TRUE,
-                plotOutput(outputId = "histCntrlPlot3"),
-                sidebar = boxSidebar(
-                  id = "hist_sidebar3",
-                  width = 40,
-                  sliderInput(inputId = "slider3",
-                              label = "Number of observations:",
-                              min = 1,
-                              max = 100,
-                              value = 50)
-                ) # end boxSidebar
-              ),
-              box(
-                id = "plotlyBox3",
-                title = "Plotly plot",
-                solidHeader = TRUE,
-                status = "primary",
-                collapsible = TRUE,
-                plotlyOutput(outputId = "plotlyPlot3")
-              )
-            ) # end div
-          ) # end jqui_draggable
-        ) # end fluidRow 2 
-      ), # end tab shinyjquiMovable
+          p("Load files here")
+        )
+      ),
       
       tabItem(
         tabName = "shinyjquiServer",
@@ -220,103 +63,6 @@ server <- function(input, output, session) {
   histdata <- rnorm(500)
   plotly_data <- data.frame(x = 1:100,
                             y = rnorm(100))
-  
-  # toggle the plots
-  observe({
-    ## static
-    shinyjs::toggle(id = "histogramBox1",
-                    condition = "histogram" %in% input$showPlots1)
-    shinyjs::toggle(id = "histogramCntrlBox1",
-                    condition = "histogramCntrl" %in% input$showPlots1)
-    shinyjs::toggle(id = "plotlyBox1",
-                    condition = "plotly" %in% input$showPlots1)
-    
-    ## shinyjquiSortable
-    shinyjs::toggle(id = "histogramBox2",
-                    condition = "histogram" %in% input$showPlots2)
-    shinyjs::toggle(id = "histogramCntrlBox2",
-                    condition = "histogramCntrl" %in% input$showPlots2)
-    shinyjs::toggle(id = "plotlyBox2",
-                    condition = "plotly" %in% input$showPlots2)
-    
-    ## shinyjquiMovable
-    shinyjs::toggle(id = "histogramBox3",
-                    condition = "histogram" %in% input$showPlots3)
-    shinyjs::toggle(id = "histogramCntrlBox3",
-                    condition = "histogramCntrl" %in% input$showPlots3)
-    shinyjs::toggle(id = "plotlyBox3",
-                    condition = "plotly" %in% input$showPlots3)
-  })
-  
-  
-  #### Static ####
-  
-  ###### plots for Static ######
-  # histogram
-  output$histPlot1 <- renderPlot({
-    hist(histdata)
-  })
-  
-  # histogram with controls
-  output$histCntrlPlot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider1)]
-    hist(data)
-  })
-  
-  # plotly plot
-  output$plotlyPlot1 <- renderPlotly({
-    plotly_data |>
-      plot_ly(x = ~x,
-              y = ~y) |>
-      add_markers()
-    
-  })
-  
-  #### shinyjquiSortable ####
-  
-  ###### plots for shinyjquiSortable ######
-  # histogram
-  output$histPlot2 <- renderPlot({
-    hist(histdata)
-  })
-  
-  # histogram with controls
-  output$histCntrlPlot2 <- renderPlot({
-    data <- histdata[seq_len(input$slider2)]
-    hist(data)
-  })
-  
-  # plotly plot
-  output$plotlyPlot2 <- renderPlotly({
-    plotly_data |>
-      plot_ly(x = ~x,
-              y = ~y) |>
-      add_markers()
-    
-  })
-  
-  #### shinyjquiMovable ####
-  
-  ###### plots for shinyjquiMovable ######
-  # histogram
-  output$histPlot3 <- renderPlot({
-    hist(histdata)
-  })
-  
-  # histogram with controls
-  output$histCntrlPlot3 <- renderPlot({
-    data <- histdata[seq_len(input$slider3)]
-    hist(data)
-  })
-  
-  # plotly plot
-  output$plotlyPlot3 <- renderPlotly({
-    plotly_data |>
-      plot_ly(x = ~x,
-              y = ~y) |>
-      add_markers()
-    
-  })
   
   #### shinyjquiServer ####
   
